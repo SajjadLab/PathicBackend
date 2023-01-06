486761608b177f015b3f233f93e818cc0aaad360
SajjadHub <alkazzazsajjad@gmail.com>
2023-01-05 22:27:47 -0800

    mongoose connection

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
+
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
+const uri = process.env.DB;
 
 app.get("/", (req, res) => {
   res.send("hello");
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
+  try {
+    _db = await mongoose.connect(
+      uri,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      () => console.log(" Mongoose is connected"))
+    }
+    catch(err) {
+      console.log(err);
+    };
   console.log("Running on port", port);
 });
